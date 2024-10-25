5b7575ab275788908312ec22f797120c96b0ab2e
Mukul Bhatt <bhattmukul2006@gmail.com>
2024-10-25 05:52:38 +0530

    Update LoginBackend.py

--- CS Project 2024-25/LoginBackend.py
+++ CS Project 2024-25/LoginBackend.py
@@ -1,36 +1,57 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import mysql.connector as c
+
+app = Flask(__name__)
+CORS(app)
+
+# Database connection
 myconn = c.connect(host="localhost", user="root", password="1234", database="Prod_Reccomend_Sys")
 mycur = myconn.cursor()
 
-# Function to create a new account
-def Register_acc():
-    uID = int(input("Enter Unique ID assigned: "))
-    mycur.execute("SELECT * FROM User_Login WHERE uID=%s", (uID,))
-    account = mycur.fetchone()
-    
-    if account is None:
-        Name = input("Enter Your Name: ")
-        Password = int(input("Create Password: "))
-        Email_ID = int(input("Enter Your Email ID: "))
-        Contact_No= int(input("Enter Your Contact No: "))
-        sql = "INSERT INTO UserLogin(uID, Name, Password, Email_ID,Contact_No) VALUES (%s, %s, %s, %s,%s)"
-        val = (uID, Name, Password, Email_ID,Contact_No)
-        mycur.execute(sql, val)
-        myconn.commit()
-        print("Account Created Successfully!!")
-    else:
-        print("Account already Exists")
+@app.route('/register', methods=['POST'])
+def register_acc():
+    try:
+        data = request.json
+        uID = int(data.get('uID'))
+        Name = data.get('Name')
+        Password = data.get('Password')
+        Email_ID = data.get('Email_ID')
+        Contact_No = data.get('Contact_No')
 
-def Login_acc():
-    uid = int(input("Enter Unique ID Assigned: "))
-    mycur.execute("SELECT * FROM User_Login WHERE uID=%s", (uID,))
-    account = mycur.fetchone()
-    
-    if account is not None:
-        password = int(input("Enter Your Password: "))
-        if account[2] == password:
-            print(f"You Have Been Registered: {User_login[1]}")
+        mycur.execute("SELECT * FROM User_Login WHERE uID=%s", (uID,))
+        account = mycur.fetchone()
+
+        if account is None:
+            sql = "INSERT INTO User_Login (uID, Name, Password, Email_ID, Contact_No) VALUES (%s, %s, %s, %s, %s)"
+            val = (uID, Name, Password, Email_ID, Contact_No)
+            mycur.execute(sql, val)
+            myconn.commit()
+            return jsonify({"message": "Account Created Successfully!"}), 201
         else:
-            print("Wrong Password")
-    else:
-        print("Account does not exist")
+            return jsonify({"message": "Account already exists"}), 400
+    except Exception as e:
+        return jsonify({"message": f"An error occurred: {str(e)}"}), 500
+
+@app.route('/login', methods=['POST'])
+def login_acc():
+    try:
+        data = request.json
+        uID = int(data.get('uID'))
+        Password = data.get('Password')
+
+        mycur.execute("SELECT * FROM User_Login WHERE uID=%s", (uID,))
+        account = mycur.fetchone()
+
+        if account is not None:
+            if account[2] == Password:  # Assuming Password is the third column
+                return jsonify({"message": f"You Have Been Logged In: {account[1]}"}), 200
+            else:
+                return jsonify({"message": "Wrong Password"}), 401
+        else:
+            return jsonify({"message": "Account does not exist"}), 404
+    except Exception as e:
+        return jsonify({"message": f"An error occurred: {str(e)}"}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
